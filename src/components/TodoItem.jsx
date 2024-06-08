ea0db041a57e57eb100e7cc3470d81cd983aec5e
adindacamila <sekarauliaadinda@gmail.com>
2024-06-09 03:13:16 +0700

    Develop toggle todo feature

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,18 +1,39 @@
 import React from 'react'
 
-const TodoItem = ({ todo }) => {
-    return (
-      <div style={styles.todoItem}>
-        <p>{todo.title}</p>
-      </div>
-    )
+const TodoItem = ({ todo, toggleCompleted }) => {
+  const getTodoTitleStyle = () => {
+    if (todo.completed === true) {
+      return { textDecoration: 'line-through' }
+    } else {
+      return { textDecoration: 'none' }
+    }
   }
-  
-  const styles = {
-    todoItem: {
-      border: '2px solid #f4f4f4',
-      fontSize: '24px',
-    },
-  }
-  
-  export default TodoItem
+
+  return (
+    <div style={styles.todoItem}>
+      <input
+        type="checkbox"
+        style={styles.checkbox}
+        onChange={() => toggleCompleted(todo.id)}
+      />
+      <p style={getTodoTitleStyle()}>{todo.title}</p>
+    </div>
+  )
+}
+
+const styles = {
+  todoItem: {
+    border: '2px solid #f4f4f4',
+    fontSize: '24px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  checkbox: {
+    marginRight: '10px',
+    height: '18px',
+    width: '18px',
+  },
+}
+
+export default TodoItem
